d41264dfdb743ca23ada7e3bb30e72aaea51ad42
Hasan Genc <hasangencx@hotmail.com>
2019-08-24 13:01:21 +0300

    Use Attribute Macros to Define Routes

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,31 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, get, post};
 
-fn test(req: HttpRequest) -> impl Responder {
+#[get("/")]
+fn home(req: HttpRequest) -> impl Responder {
+    let word = req.match_info().get("word").unwrap_or("Blockchain");
+    format!("Hello {}!", &word)
+}
+
+#[get("/blocks")]
+fn blocks(req: HttpRequest) -> impl Responder {
+    let word = req.match_info().get("word").unwrap_or("Blockchain");
+    format!("Hello {}!", &word)
+}
+
+#[post("/mine-block")]
+fn mine_block(req: HttpRequest) -> impl Responder {
+    let word = req.match_info().get("word").unwrap_or("Blockchain");
+    format!("Hello {}!", &word)
+}
+
+#[get("/peers")]
+fn peers(req: HttpRequest) -> impl Responder {
+    let word = req.match_info().get("word").unwrap_or("Blockchain");
+    format!("Hello {}!", &word)
+}
+
+#[post("/add-peer")]
+fn add_peer(req: HttpRequest) -> impl Responder {
     let word = req.match_info().get("word").unwrap_or("Blockchain");
     format!("Hello {}!", &word)
 }
@@ -8,12 +33,11 @@ fn test(req: HttpRequest) -> impl Responder {
 pub fn start() {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(test))
-            .route("/{word}", web::get().to(test))
-            .route("/blocks", web::get().to(test))
-            .route("/mine-block", web::post().to(test))
-            .route("/peers", web::get().to(test))
-            .route("/add-peer", web::post().to(test))
+            .service(home)
+            .service(blocks)
+            .service(mine_block)
+            .service(peers)
+            .service(add_peer)
     })
         .bind("127.0.0.1:1461")
         .expect("Can not bind to port 1461")
